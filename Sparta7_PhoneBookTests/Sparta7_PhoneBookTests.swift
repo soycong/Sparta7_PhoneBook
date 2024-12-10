41f9ce4dee23eb010410f0f666f002ea9013884b
soy <soysauce1223@naver.com>
2024-12-10 11:02:51 +0900

    Initial Commit

---
+++ Sparta7_PhoneBookTests/Sparta7_PhoneBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sparta7_PhoneBookTests.swift
+//  Sparta7_PhoneBookTests
+//
+//  Created by seohuibaek on 12/10/24.
+//
+
+import Testing
+@testable import Sparta7_PhoneBook
+
+struct Sparta7_PhoneBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Sparta7_PhoneBookUITests/Sparta7_PhoneBookUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Sparta7_PhoneBookUITestsLaunchTests.swift
+//  Sparta7_PhoneBookUITests
+//
+//  Created by seohuibaek on 12/10/24.
+//
+
+import XCTest
+
+final class Sparta7_PhoneBookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
