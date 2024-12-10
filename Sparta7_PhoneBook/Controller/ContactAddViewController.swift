ad270214f9117f789436714b36e16679ae4bee9d
soy <soysauce1223@naver.com>
2024-12-10 16:16:55 +0900

    [add] 번호 추가를 위한 ContactAddView, ContactAddViewController 생성

---
+++ Sparta7_PhoneBook/Controller/ContactAddViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ContactAddViewController.swift
+//  Sparta7_PhoneBook
+//
+//  Created by seohuibaek on 12/10/24.
+//
+import UIKit
+
+class ContactAddViewController: UIViewController {
+    
+}
---
+++ Sparta7_PhoneBook/View/ContactAddView.swift
@@ -0,0 +1,12 @@
+//
+//  ContactAddView.swift
+//  Sparta7_PhoneBook
+//
+//  Created by seohuibaek on 12/10/24.
+//
+import UIKit
+import SnapKit
+
+class ContactAddView: UIView {
+    
+}
