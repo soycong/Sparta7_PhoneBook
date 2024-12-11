68e3c977f3ed3170ee1c22d382cbb2bca1ba0f71
soy <soysauce1223@naver.com>
2024-12-11 13:44:43 +0900

    [feat] CoreData 추가

---
+++ PhoneBook+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PhoneBook+CoreDataClass.swift
+//  Sparta7_PhoneBook
+//
+//  Created by seohuibaek on 12/11/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PhoneBook)
+public class PhoneBook: NSManagedObject {
+
+}
