59365c14e90324f327e151426932af4ecde6c1e1
soy <soysauce1223@naver.com>
2024-12-11 19:33:40 +0900

    [refactor] 폴더링작업 진행

--- Sparta7_PhoneBook/Network/PokemonImageService.swift
+++ Sparta7_PhoneBook/Network/PokemonImageService.swift
@@ -21,3 +21,14 @@ class PokemonImageService {
     }
 }
 
+struct PokemonImageModel: Codable {
+    let id: Int
+    let name: String
+    let height: Int
+    let weight: Int
+    let sprites: Sprites
+}
+
+struct Sprites: Codable {
+    let front_default: String
+}
